22bd6c1f143dd316649db8a5ea4456478a4ca580
JGMelon22 <jgvasconcellos22@gmail.com>
2024-01-20 19:27:24 -0300

    chore: including basic validation with Data Annotation for TypicalPlaceInput

--- DTOs/TypicalPlace/TypicalPlaceInput.cs
+++ DTOs/TypicalPlace/TypicalPlaceInput.cs
@@ -1,3 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FoodShop.DTOs.TypicalPlace;
 
-public record TypicalPlaceInput(string Country);
+public record TypicalPlaceInput(
+    [Required(AllowEmptyStrings = false, ErrorMessage = "The Food Typical Place must be informed!")]
+    [MinLength(2, ErrorMessage = "Food Food Typical Places must be greater than 2 characters!")]
+    [MaxLength(100, ErrorMessage = "Food Food Typical Places can not exceed 100 characters!")]
+    string Country
+);
