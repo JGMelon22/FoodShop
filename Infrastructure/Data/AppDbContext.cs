8b323d18501489f32e9155142dd5a67d0a45a48d
JGMelon22 <jgvasconcellos22@gmail.com>
2024-01-20 15:17:35 -0300

    chore: developing AppDbContext

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+using FoodShop.Domain.Entities;
+using FoodShop.Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodShop.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Food> Foods { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<TypicalPlace> TypicalPlaces { get; set; }
+
+    public DbSet<FoodTypicalPlace> FoodTypicalPlaces { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new FoodConfiguration());
+        modelBuilder.ApplyConfiguration(new FoodConfiguration());
+        modelBuilder.ApplyConfiguration(new FoodConfiguration());
+        modelBuilder.ApplyConfiguration(new FoodConfiguration());
+    }
+}
